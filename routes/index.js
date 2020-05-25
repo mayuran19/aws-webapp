6f726a73e6aa12573b84629e6edc87d3368a5331
Mayuan <mayuran19@gmail.com>
2020-05-25 21:49:10 +0800

    first

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var ip = require("ip");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  res.render('index', { title: 'Express' + ip.address() });
+});
+
+module.exports = router;
